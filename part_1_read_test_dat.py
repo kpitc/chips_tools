dd6af07158ea4bde049e30e2a2c999c9140b5549
kpitc <70523656+kpitc@users.noreply.github.com>
2025-02-11 22:58:44 -0500

    added pfgd_test.txt and sc of level

--- part_1_read_test_dat.py
+++ part_1_read_test_dat.py
@@ -3,6 +3,13 @@ import cc_dat_utils
 #Part 1
 input_dat_file = "data/pfgd_test.dat"
 
+level_pack = cc_dat_utils.make_cc_level_pack_from_dat(input_dat_file)
+
+print(str(level_pack))
 
 #Use cc_dat_utils.make_cc_level_pack_from_dat() to load the file specified by input_dat_file
 #print the resulting data
+output_file_path = "data/ppfgd_test.txt"
+with open(output_file_path, "w") as file:
+    file.write(str(level_pack))
+
